3c72036c15adb9284c4f78dcece0f2080740f625
Nate Truelove <trueloven@gmail.com>
2019-07-31 13:09:21 -0700

    Removed source from Dockerfile

--- anacapa_ex1.sh
+++ anacapa_ex1.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-source /usr/local/anacapa/singularity-files/environment
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 DB="$DIR/anacapa/Anacapa_db"
